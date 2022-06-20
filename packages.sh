dce1de53f084c7e6b67468ea32ce6211d6614f5f
Arsene Awounou <developer.awounou@gmail.com>
2022-06-20 10:12:58 +0100

    install dependancies

---
+++ packages.sh
@@ -0,0 +1,6 @@
+#Install Jest using:
+npm install --save-dev jest
+#Install Babel using:
+npm install --save-dev babel-jest @babel/core @babel/preset-env
+#Install ESLint using:
+npm install --save-dev eslint
