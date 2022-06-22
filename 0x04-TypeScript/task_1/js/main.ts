e8311a9e1f430dea678cd09d300bd76a713755a6
Arsene Awounou <developer.awounou@gmail.com>
2022-06-22 23:22:31 +0100

    Printing teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,14 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = function (
+  firstName: string,
+  lastName: string
+): string {
+  return `${firstName[0]}. ${lastName}`;
+};
