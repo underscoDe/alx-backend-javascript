6bff0a93c1bf4785f8df21b5f2d76447baea9b9d
Arsene Awounou <developer.awounou@gmail.com>
2022-06-22 15:04:55 +0100

    A Course, Getters, and Setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,50 @@
+/* eslint no-underscore-dangle: ["error", {"allow": ["_name", "_length", "_students"] }] */
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(Name) {
+    if (typeof Name === 'string') {
+      this._name = Name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = Name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(Length) {
+    if (typeof Length === 'number') {
+      this._length = Length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(Students) {
+    if (typeof Students === 'object') {
+      for (const student in Students) {
+        if (typeof student !== 'string') {
+          throw new TypeError('Students must contain only strings');
+        }
+      }
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = Students;
+  }
+}
