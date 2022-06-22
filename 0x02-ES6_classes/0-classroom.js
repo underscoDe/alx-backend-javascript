eb8e6815aefabef498e6f309a8a5a7511a508f31
Arsene Awounou <developer.awounou@gmail.com>
2022-06-22 14:56:01 +0100

    You used to attend a place like this at some point

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,7 @@
+/* eslint no-underscore-dangle: ["error", {"allow": ["_maxStudentsSize"] }] */
+
+export default class classRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
