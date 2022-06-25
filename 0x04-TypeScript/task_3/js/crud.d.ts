e748664a919fa0f324038abb6653e6cdf088f379
Arsene Awounou <developer.awounou@gmail.com>
2022-06-25 18:50:30 +0100

    Ambient Namespaces

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,4 @@
+import { RowID, RowElement } from './interface';
+declare function insertRow(row: RowElement): RowID;
+declare function deleteRow(rowId: RowID): void;
+declare function updateRow(rowId: RowID, row: RowElement): RowID;
---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,16 @@
+import { RowID, RowElement } from "./interface";
+import * as CRUD from "./crud";
+
+const row: RowElement = {
+  firstName: "Guillaume",
+  lastName: "Salva",
+}
+
+const newRowID: RowID = CRUD.insertRow(row);
+const updatedRow: RowElement = {
+  ...row,
+  age: 23,
+}
+
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
