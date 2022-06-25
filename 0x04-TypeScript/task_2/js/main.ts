94c7ab9771c9083644b8604a636e78488812d3bb
Arsene Awounou <developer.awounou@gmail.com>
2022-06-25 18:47:52 +0100

    Creating functions specific to employees

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -42,3 +42,17 @@ const createEmployee = (salary: number | string): Director | Teacher => {
   if (typeof salary === "number" && salary < 500) return new Teacher();
   return new Director();
 };
+
+const isDirector = (employee: Director | Teacher): employee is Director => {
+  return employee instanceof Director;
+};
+
+const executeWork = (employee: Director | Teacher): string => {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+};
+
+
