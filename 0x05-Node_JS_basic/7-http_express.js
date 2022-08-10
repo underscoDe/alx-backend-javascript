8366ec2639ccb35e82489fbe9dd7051b706cfb52
Arsene Awounou <developer.awounou@gmail.com>
2022-08-10 08:40:55 +0100

    fix lint errors

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -5,11 +5,6 @@ const app = express();
 const PORT = 1245;
 const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
 
-/**
- * Counts the students in a CSV data file.
- * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- */
 const countStudents = (dataPath) => new Promise((resolve, reject) => {
   if (!dataPath) {
     reject(new Error('Cannot load the database'));
@@ -26,14 +21,14 @@ const countStudents = (dataPath) => new Promise((resolve, reject) => {
         const dbFieldNames = fileLines[0].split(',');
         const studentPropNames = dbFieldNames.slice(
           0,
-          dbFieldNames.length - 1
+          dbFieldNames.length - 1,
         );
 
         for (const line of fileLines.slice(1)) {
           const studentRecord = line.split(',');
           const studentPropValues = studentRecord.slice(
             0,
-            studentRecord.length - 1
+            studentRecord.length - 1,
           );
           const field = studentRecord[studentRecord.length - 1];
           if (!Object.keys(studentGroups).includes(field)) {
@@ -41,20 +36,20 @@ const countStudents = (dataPath) => new Promise((resolve, reject) => {
           }
           const studentEntries = studentPropNames.map((propName, idx) => [
             propName,
-            studentPropValues[idx]
+            studentPropValues[idx],
           ]);
           studentGroups[field].push(Object.fromEntries(studentEntries));
         }
 
         const totalStudents = Object.values(studentGroups).reduce(
-          (pre, cur) => (pre || []).length + cur.length
+          (pre, cur) => (pre || []).length + cur.length,
         );
         reportParts.push(`Number of students: ${totalStudents}`);
         for (const [field, group] of Object.entries(studentGroups)) {
           reportParts.push([
             `Number of students in ${field}: ${group.length}.`,
             'List:',
-            group.map((student) => student.firstname).join(', ')
+            group.map((student) => student.firstname).join(', '),
           ].join(' '));
         }
         resolve(reportParts.join('\n'));
